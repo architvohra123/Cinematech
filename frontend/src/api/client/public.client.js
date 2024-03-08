d0a7ad4817fcdbeec2872a962a0000c7803fd901
Animesh <animeshsharma42002@gmail.com>
2024-03-08 20:07:18 +0530

    structure created

--- frontend/src/api/client/public.client.js
+++ frontend/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL ="https://moonflix-api.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
@@ -21,7 +21,7 @@ publicClient.interceptors.request.use(async config => {
 
 publicClient.interceptors.response.use((response) => {
   if (response && response.data) return response.data;
-  return response;
+
 }, (err) => {
   throw err.response.data;
 });
